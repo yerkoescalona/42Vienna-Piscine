9c8d8566a6a2e0cc14c7f1e0a8eb5236a78bf1d2
Yerko Escalona <35448591+yerkoescalona@users.noreply.github.com>
2023-07-29 13:12:09 +0200

    Update ft_strcmp2.c

--- c03/ex00/ft_strcmp2.c
+++ c03/ex00/ft_strcmp2.c
@@ -14,8 +14,12 @@ int	ft_strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2)
 	{
-		if (*s1++ != *s2++)
+		if (*s1 != *s2)
+		{
 			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
 	}
 	return (*s1 - *s2);
 }
